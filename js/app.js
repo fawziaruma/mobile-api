1ffc52d7867329fd1abaf026da253dcf5d903206
Fawzia Ruma <fawziaruma0652@gmail.com>
2023-04-02 19:59:18 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,19 @@ const loadData = (searchText) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=samsung`
     fetch(url)
     .then(res => res.json())
-    .then(data =>console.log(data))
+    .then(data =>displayData(data.data))
+}
+const displayData = mobiles =>{
+    const phnContainer = document.getElementById('phn-container')
+    mobiles.forEach(mobile =>{
+        console.log(mobile);
+        const creatDiv = document.createElement('div')
+        creatDiv.innerHTML = `
+        <img src="${mobile.image}" alt="">
+        <h3> name: ${mobile.phone_name}</h3>
+        `
+        phnContainer.appendChild(creatDiv)
+    })
+
 }
 loadData()
